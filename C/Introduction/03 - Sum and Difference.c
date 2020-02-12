eac6f43eed74cbf5b35e77d3e409d143353fa21b
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-12 00:54:03 -0500

    Update 03 - Sum and Difference.c

---
+++ C/Introduction/03 - Sum and Difference.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    a = b = 0;
+    float c,d;
+    c = d = 0.0;
+	scanf("%d%d\n%f%f",&a,&b,&c,&d);
+
+    printf("%d %d\n%.1f %.1f",(a+b),(a-b),(c+d),(c-d));
+    
+    return 0;
+}
+
