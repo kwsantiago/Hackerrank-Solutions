3f9320a931be53eca1c4d16dfd14b72ffe20f11f
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-14 23:24:43 -0500

    Create 04 - Functions.c

---
+++ C/Introduction/04 - Functions.c
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max_of_four(int a, int b, int c, int d)
+{
+    int m = a;
+    if (b > m) m = b;
+    if (c > m) m = c;
+    if (d > m) m = d;
+    return m;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+
+    return 0;
+}
+
+
+
