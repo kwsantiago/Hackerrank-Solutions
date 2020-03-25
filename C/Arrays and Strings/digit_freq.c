f03ae96b1e53ec73759dcdf6582fd359bc7f4efb
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-03-24 23:54:50 -0400

    Create digit_freq.c

---
+++ C/Arrays and Strings/digit_freq.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int c, num[10], i;
+    for(i = 0; i <= 10; i++)
+        num[i] = 0;
+    while((c = getchar()) != '\n'){
+        switch(c){
+            case '0':
+                num[0]++;
+                break;
+            case '1':
+                num[1]++;
+                break;
+            case '2':
+                num[2]++;
+                break;
+            case '3':
+                num[3]++;
+                break;
+            case '4':
+                num[4]++;
+                break;
+            case '5':
+                num[5]++;
+                break;
+            case '6':
+                num[6]++;
+                break;
+            case '7':
+                num[7]++;
+                break;
+            case '8':
+                num[8]++;
+                break;
+            case '9':
+                num[9]++;
+                break;
+        }
+    }
+    for(i = 0; i < 10; i++)
+        printf("%d ", num[i]);
+    printf("\n");
+    return 0;
+}
