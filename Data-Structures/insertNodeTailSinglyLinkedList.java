4308ebce9751bfe121999ac5a7809de9a2196686
kwsantiago <kwsantiago@mail.usf.edu>
2020-07-01 10:18:54 -0400

    Remove Indent

--- Data-Structures/insertNodeTailSinglyLinkedList.java
+++ Data-Structures/insertNodeTailSinglyLinkedList.java
@@ -1,12 +1,12 @@
-    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data){
-        SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
-        if(head == null) return newNode;
-        else{
-            SinglyLinkedListNode currentNode = head;
-            while(currentNode.next != null)
-                currentNode = currentNode.next;
-            currentNode.next = newNode;
-            currentNode.next.next = null;
-        }
-        return head;
+static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data){
+    SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+    if(head == null) return newNode;
+    else{
+        SinglyLinkedListNode currentNode = head;
+        while(currentNode.next != null)
+            currentNode = currentNode.next;
+        currentNode.next = newNode;
+        currentNode.next.next = null;
     }
+    return head;
+}
